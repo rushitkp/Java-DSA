57b771a57a0fb916f072dba79d0e5a10f8bc0cad
rushitkp <rkp14503@gmail.com>
2024-07-12 20:02:11 +0530

    Program

---
+++ Bit_MAnipulation.java
@@ -0,0 +1,8 @@
+public class Bit_MAnipulation {
+    public static void main(String[] args) {
+        System.out.println(4 & 6);
+        System.out.println(5 | 6);
+        System.out.println(5 ^ 6);
+        System.out.println(~1);
+    }
+}
