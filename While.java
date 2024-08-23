c2524f160d83677289da41c6d6401356ca62b1e5
rushitkp <rkp14503@gmail.com>
2024-08-23 19:42:35 +0530

    Program

--- While.java
+++ While.java
@@ -1,7 +1,7 @@
 import java.util.Scanner;
 
 class While {
-  public static void main(String1[] args) {
+  public static void main(String[] args) {
     Scanner sc = new Scanner(System.in);
     System.out.print("Enter Number:");
     int num = sc.nextInt();
